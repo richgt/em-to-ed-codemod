fa8cb42b3f17efd8caa2aeaa34d515b4c7da175a
Pat O'Callaghan <patocallaghan@gmail.com>
2019-01-17 09:13:20 +0000

    Update transform name

--- transforms/em-to-ed/__testfixtures__/model-has-many.output.js
+++ transforms/em-to-ed/__testfixtures__/model-has-many.output.js
@@ -4,15 +4,15 @@ import Admin from 'embercom/models/admin';
 import Team from 'embercom/models/team';
 
 export default DS.Model.extend({
-  tags: DS.attr('ember-model-array', {
+  tags: DS.attr('ember-model-has-many', {
     modelClass: Tag,
     embedded: true,
   }),
-  admin: DS.attr('ember-model-array', {
+  admin: DS.attr('ember-model-has-many', {
     modelClass: Admin,
     embedded: false,
   }),
-  team: DS.attr('ember-model-array', {
+  team: DS.attr('ember-model-has-many', {
     modelClass: Team,
     embedded: false,
   }),
